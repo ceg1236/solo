aadc9b4819d22f3bf9d6fb949c93f87b2fdb2021
Carl Goldberg <carlgoldberg1236@gmail.com>
2014-12-09 17:28:58 -0800

    Refactor - create map.js, bower_comp, moving towards Angular

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
-// Clicking a circle pops up infobox w time of session  
-// Display all locations in FB on initial map 
-// Over repeats loading of current positions in FB - draws circles on every click 
-//  --> Right now every DOMevent is triggering initialize (bottom line)
-//  -->  If circle has been drawn, log as already drawn to avoid doing again, 
-//  -->  Make sure to remove it when the circle ends 
-//  --> Over rendering due to click event and DOM reload -- when click event disabled, we elim extra render
-//     -- but have lost ability to remove circle on next click and after timeOut... 
-// Add current loc button 
-// Further ideas: Keep track of stats (total number, total time, broken down by region)
-//                Visualize the data - merge the circles in an area
+/* Clicking a circle pops up infobox w time of session  
+ Display all locations in FB on initial map 
+ Over repeats loading of current positions in FB - draws circles on every click 
+  --> Right now every DOMevent is triggering initialize (bottom line)
+  -->  If circle has been drawn, log as already drawn to avoid doing again, 
+  -->  Make sure to remove it when the circle ends 
+  --> Over rendering due to click event and DOM reload -- when click event disabled, we elim extra render
+     -- but have lost ability to remove circle on next click and after timeOut... 
+ Add current loc button 
+ Further ideas: Keep track of stats (total number, total time, broken down by region)
+                Visualize the data - merge the circles in an area
+*/
 
 
-var map, geocoder, pos, 
-    logged = {},
-    firebase = new Firebase("https://blinding-fire-9877.firebaseio.com/"); 
+// var map, 
+//     geocoder, 
+//     pos, 
+//     logged = {},
+//     firebase = new Firebase("https://blinding-fire-9877.firebaseio.com/");  // Hard-coded
 
-function initialize() {
-  geocoder = new google.maps.Geocoder();
-  var mapOptions = {
-    zoom: 17
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+// function initialize() {
+//   geocoder = new google.maps.Geocoder();
+//   var mapOptions = {
+//     zoom: 17
+//   };
+//   map = new google.maps.Map(document.getElementById('map-canvas'),
+//       mapOptions);
 
-  findLocation();
-  render();
-}
+//   findLocation();
+//   render();
+// }
 
-var render = function() {
+var renderUsers = function(map) {
 
   circle.setVisible(false);
   console.log('renderrr');
@@ -56,43 +59,43 @@ var render = function() {
 };
 
   // Try HTML5 geolocation
-var findLocation = function() {
-  if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-      pos = new google.maps.LatLng(position.coords.latitude,
-                                   position.coords.longitude);
+// var findLocation = function() {
+//   if(navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(function(position) {
+//       pos = new google.maps.LatLng(position.coords.latitude,
+//                                    position.coords.longitude);
 
-    var marker = new google.maps.Marker({
-      position: pos,
-      map: map,
-      title: 'Hello World!'
-    });
+//     var marker = new google.maps.Marker({
+//       position: pos,
+//       map: map,
+//       title: 'Hello World!'
+//     });
 
-    map.setCenter(pos);
+//     map.setCenter(pos);
 
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleNoGeolocation(false);
-  }
-}
+//     });
+//   } else {
+//     // Browser doesn't support Geolocation
+//     handleNoGeolocation(false);
+//   }
+// }
 
-function handleNoGeolocation(errorFlag) {
-  if (errorFlag) {
-    var content = 'Error: The Geolocation service failed.';
-  } else {
-    var content = 'Error: Your browser doesn\'t support geolocation.';
-  }
+// function handleNoGeolocation(errorFlag) {
+//   if (errorFlag) {
+//     var content = 'Error: The Geolocation service failed.';
+//   } else {
+//     var content = 'Error: Your browser doesn\'t support geolocation.';
+//   }
 
-  var options = {
-    map: map,
-    position: new google.maps.LatLng(37.775, 122.419),
-    content: content
-  };
+//   var options = {
+//     map: map,
+//     position: new google.maps.LatLng(37.775, 122.419),
+//     content: content
+//   };
 
-  var infowindow = new google.maps.InfoWindow(options);
-  map.setCenter(options.position);
-}
+//   var infowindow = new google.maps.InfoWindow(options);
+//   map.setCenter(options.position);
+// }
 
 var circle, emission,
   meditating = false; 
@@ -114,11 +117,11 @@ $(function() {
         radius: 24 
       });
       emission = firebase.push({'position': pos });
-    setTimeout(function() {         // Circle disappears after 25 mins
-      emission.remove();
-      render(); 
-      circle.setVisible(false);
-    }, 1500000);
+      setTimeout(function() {         // Circle disappears after 25 mins
+        emission.remove();
+        renderUsers(); 
+        circle.setVisible(false);
+      }, 1500000);
     } else {                        // Toggle circle off 
       emission.remove(); 
       circle.setVisible(false);
@@ -131,22 +134,22 @@ $(function() {
 
 
 // Find location
-function codeAddress() {
-  geocoder = new google.maps.Geocoder();
-  var address = document.getElementById('address').value;
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: map,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
+// function codeAddress() {
+//   geocoder = new google.maps.Geocoder();
+//   var address = document.getElementById('address').value;
+//   geocoder.geocode( { 'address': address}, function(results, status) {
+//     if (status == google.maps.GeocoderStatus.OK) {
+//       map.setCenter(results[0].geometry.location);
+//       var marker = new google.maps.Marker({
+//         map: map,
+//         position: results[0].geometry.location
+//       });
+//     } else {
+//       alert('Geocode was not successful for the following reason: ' + status);
+//     }
+//   });
+// }
 
 
-google.maps.event.addDomListener(window, 'load', initialize);
+// google.maps.event.addDomListener(window, 'load', initialize);
 
